8d968065d9ae9eec43ad1fe969889d7526270b00
mo-hamedaziz <mohamedaziz.bchini@insat.ucar.tn>
2025-01-28 14:29:20 +0100

    Injected job offers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { JobOffersComponent } from './components/job-offers/job-offers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomePageComponent
+    WelcomePageComponent,
+    NavbarComponent,
+    JobOffersComponent
   ],
   imports: [
     BrowserModule,
