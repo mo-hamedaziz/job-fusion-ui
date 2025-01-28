cf993018862e773413d232f6f09d7267495c2268
mo-hamedaziz <mohamedaziz.bchini@insat.ucar.tn>
2025-01-28 14:28:29 +0100

    Added Navbar component

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+  isAuthenticated$ = this.authService.isAuthenticated$;
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  login() {
+    this.authService.login();
+    this.router.navigate(['/welcome']);
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/landing']);
+  }
+
+}
