0279856429e4dca0abfcecd75a75ccefa8bf6417
elyes <elyes.charfeddine@insat.ucar.tn>
2025-01-30 15:13:35 +0100

    page recruiter

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
-
+import { SignupComponent } from './components/auth/signup/signup.component';
 const routes: Routes = [
+  { path: 'signup', component: SignupComponent },
   { path: 'welcome', component: WelcomePageComponent },
   { path: '', redirectTo: '/welcome', pathMatch: 'full' },
   { path: '**', redirectTo: '/welcome' },
+
+/*   { path: 'dashboard', component: DashboardComponent }, */
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { JobOffersComponent } from './components/job-offers/job-offers.component';
-
+import { SignupComponent } from './components/auth/signup/signup.component';
+import { JobPostComponent } from './job-post/job-post.component';
+import { JobDetailsComponent } from './job-details/job-details.component';
+import { RecruiterJobListComponent } from './recruiter-job-list/recruiter-job-list.component';
+import { RecruiterJobFormComponent } from './components/recruiter-job-form/recruiter-job-form/recruiter-job-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RecruiterApgeComponent } from './pages/recruiter-apge/recruiter-apge.component';
 @NgModule({
   declarations: [
     AppComponent,
     WelcomePageComponent,
     NavbarComponent,
-    JobOffersComponent
+    JobOffersComponent,
+    SignupComponent,
+    JobPostComponent,
+    JobDetailsComponent,
+    RecruiterJobListComponent,
+    RecruiterJobFormComponent,
+    RecruiterApgeComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
